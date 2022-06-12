7b17347971d2d4241ed1a66776a740411cab0065
LaFusew <antoine.oddoz@gmail.com>
2022-06-13 00:46:11 +0200

    [+] Sample CRUD

--- data/models/User.go
+++ data/models/User.go
@@ -13,3 +13,38 @@ type User struct {
 	ProfilePicture string    `gorm:"size:200;" json:"profile_picture"`
 	Desc           string    `gorm:"size:255;" json:"desc"`
 }
+
+func (u *User) Create(db *gorm.DB) (*User, error) {
+	err := db.Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+
+	return u, err
+}
+
+func (u *User) Update(db *gorm.DB, id uuid.UUID) (*User, error) {
+	user := &User{}
+	db.Model(&User{}).Where("id = ?", id).Updates(u).Take(&user)
+
+	return user, nil
+}
+
+func (u *User) FindById(db *gorm.DB, id uuid.UUID) (*User, error) {
+	err := db.Model(User{}).Where("id = ?", id).Take(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+
+	return u, err
+}
+
+func (u *User) FindAll(db *gorm.DB, pagination int, limit int) (*[]User, error) {
+	users := []User{}
+	err := db.Debug().Model(&User{}).Limit(limit).Offset(pagination * limit).Find(&users).Error
+	if err != nil {
+		return &[]User{}, err
+	}
+
+	return &users, err
+}
