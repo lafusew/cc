05393b51ae31ae80e0c321f451703d17ed694098
LaFusew <antoine.oddoz@gmail.com>
2022-06-12 14:43:03 +0200

    [+] struct tags

--- data/models/Account.go
+++ data/models/Account.go
@@ -2,12 +2,20 @@ package data
 
 import "gorm.io/gorm"
 
+type AccountAuthority string
+
+const (
+	OWNER AccountAuthority = "OWNER"
+	ADMIN AccountAuthority = "ADMIN"
+	USER AccountAuthority = "USER"
+)
+
 type Account struct {
 	gorm.Model
 	Name string `gorm:"size:18; not null" json:"name"`
-	UserID uint ``
-	CoinID uint
-	Authority string 
-	Balance uint
-	Scale uint
+	UserID uint `gorm:"not null" json:"user_id"`
+	CoinID uint `gorm:"not null" json:"coin_id"`
+	Authority AccountAuthority `gorm:"not null" json:"authority"`
+	Balance uint `gorm:"not null" json:"balance"`
+	Scale uint `gorm:"not null" json:"scale"`
 }
