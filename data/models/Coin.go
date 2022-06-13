04818ea4271823ae3c4d00906decc8c0451f3c90
LaFusew <antoine.oddoz@gmail.com>
2022-06-13 10:33:50 +0200

    [+] Crud coin

--- data/models/Coin.go
+++ data/models/Coin.go
@@ -15,3 +15,41 @@ type Coin struct {
 	DefaultBalance      uint      `gorm:"not null" json:"default_balance"`
 	DefaultBalanceScale uint      `gorm:"not null" json:"default_balance_scale"`
 }
+
+func (c *Coin) Create(db *gorm.DB) (*Coin, error) {
+	err := db.Create(&c).Error
+	if err != nil {
+		return &Coin{}, err
+	}
+
+	return c, err
+}
+
+func (c *Coin) Update(db *gorm.DB, id uuid.UUID) (*Coin, error) {
+	coin := &Coin{}
+	err := db.Model(&Coin{}).Where("id = ?", id).Updates(c).Take(&coin).Error
+	if err != nil {
+		return &Coin{}, err
+	}
+
+	return coin, err
+}
+
+func (c *Coin) FindById(db *gorm.DB, id uuid.UUID) (*Coin, error) {
+	err := db.Model(&Coin{}).Where("id = ?", id).Take(&c).Error
+	if err != nil {
+		return &Coin{}, err
+	}
+
+	return c, err
+}
+
+func (c *Coin) FindAll(db *gorm.DB, pagination int, limit int) (*[]Coin, error) {
+	coins := []Coin{}
+	err := db.Debug().Model(&Coin{}).Limit(limit).Offset(pagination * limit).Find(&coins).Error
+	if err != nil {
+		return &[]Coin{}, err
+	}
+
+	return &coins, err
+}
